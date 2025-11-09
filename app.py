a8d6a4214288a990b625e884fe65caf0cce0d9f6
Prabhav Sharma <prabhav.trainer@global.org.in>
2025-11-09 22:32:39 +0530

    This commit includes working version of app.py, which is giving a predicted values in json

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas as pd
-
 
 app = Flask(__name__)
 
-## Load the model
+# Load model and scaler
 regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))
 
 @app.route('/')
-def home() :
+def home():
     return render_template('home.html')
 
 @app.route('/predict_api', methods=['POST'])
-
 def predict_api():
-    data = req.json['data']    ## This will make sure whenever i hit my prediction API, the data will come in json format
-    print(data)
-    print(np.array(list(data.values()))).reshape(1, -1)
-    new_data = scaler.transform(np.array(list(data.values()))).reshape(1, -1)
-    output = regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+    
+    data = request.json['data']
+    print("Input JSON:", data)
 
-if __name__ == "__main__" :
+    input_data = np.array(list(data.values())).reshape(1, -1)
+    print("Input array:", input_data)
+
+    new_data = scaler.transform(input_data)
+    output = regmodel.predict(new_data)
+    print("Prediction:", output[0])
+
+    return jsonify({'prediction': float(output[0])})
+
+if __name__ == "__main__":
     app.run(debug=True)
