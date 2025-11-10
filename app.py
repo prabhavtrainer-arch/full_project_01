ef9f3425bac748c281e4c9cbe30a8da4d478c651
Prabhav Sharma <prabhav.trainer@global.org.in>
2025-11-10 22:14:02 +0530

    this commit inlcudes a running web app

--- app.py
+++ app.py
@@ -27,5 +27,17 @@ def predict_api():
 
     return jsonify({'prediction': float(output[0])})
 
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)
+    return render_template("home.html", prediction_text = "The predicted price is {}".format(output))
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
